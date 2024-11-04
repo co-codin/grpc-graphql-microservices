2f0a6b9caf4243b2fe57bd2cc2f5e99215d8e7be
elijah <cuiyeqing960904@gmail.com>
2024-11-04 22:15:02 +0300

    creating graph

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module go-grpc-graphql
 
-go 1.22.2
+go 1.22.5
+
+toolchain go1.22.8
+
+require (
+	github.com/99designs/gqlgen v0.17.55
+	github.com/vektah/gqlparser/v2 v2.5.18
+)
+
+require (
+	github.com/agnivade/levenshtein v1.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+)
--- graphql/graph.go
+++ graphql/graph.go
@@ -1,1 +1,31 @@
 package main
+
+type Server struct {
+	
+}
+
+func NewGraphQLServer (accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+	accountClient, err := account.NewClient(accountUrl)
+	if err != nil {
+		accountClient.Close()
+		return nil, err
+	}
+
+	catalogClient, err := catalog.NewClient(catalogUrl)
+	if err != nil {
+		catalogClient.Close()
+		return nil, err
+	}
+
+	orderClient, err := order.NewClient(orderUrl)
+	if err != nil {
+		orderClient.Close()
+		return nil, err
+	}
+
+	return &Server{
+		accountClient,
+		catalogClient,
+		orderClient,
+	}, nil
+}
