234dd9cfaf7bdf268c948f4a686b25605a09e932
elijah <cuiyeqing960904@gmail.com>
2024-11-18 18:39:59 +0300

    account repo

--- account/repository.go
+++ account/repository.go
@@ -1,1 +1,49 @@
 package account
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Repository interface {
+	Close()
+	PutAccount(ctx context.Context, a Account) error
+	GetAccountByID(ctx context.Context, id string) (*Account, error)
+	ListAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error)
+}
+
+type postgresRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresRepository(url string) (Repository, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return &postgresRepository{db}, nil
+}
+
+func (r *postgresRepository) Close() {
+	r.db.Close()
+}
+
+func (r *postgresRepository) Ping() error {
+	return r.db.Ping()
+}
+
+func (r *postgresRepository) PutAccount(ctx context.Context, a Account) error {
+	return nil
+}
+
+func (r *postgresRepository) GetAccountByID(ctx context.Context, id string) (*Account, error) {
+	return nil, nil
+}
+
+func (r *postgresRepository) ListAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error) {
+	return nil, nil
+}
--- account/service.go
+++ account/service.go
@@ -1,1 +1,9 @@
 package account
+
+type Service interface {
+}
+
+type Account struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
