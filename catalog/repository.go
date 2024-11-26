817ac7ba1381ecb66b6a83e52fd0023939441813
elijah <cuiyeqing960904@gmail.com>
2024-11-26 23:08:50 +0300

    connect to elastic

--- catalog/repository.go
+++ catalog/repository.go
@@ -1,1 +1,25 @@
 package catalog
+
+import (
+	"context"
+	"errors"
+
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+var (
+	ErrNotFound = errors.New("Entity not found")
+)
+
+type Repository interface {
+	Close()
+	PutProduct(ctx context.Context, p Product) error
+	GetProductByID(ctx context.Context, id string) (*Product, error)
+	ListProducts(ctx context.Context, skip uint64, take uint64) ([]Product, error)
+	ListProductsWithIDs(ctx context.Context, ids []string) ([]Product, error)
+	SearchProducts(ctx context.Context, query string, skip uint64, take uint64) ([]Product, error)
+}
+
+type elasticRepository struct {
+    client *elastic.Client
+}
